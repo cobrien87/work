d5a8d8e72566a9abc0c76edea921ada4a7e1ea79
Chris O'Brien <cobrien528@gmail.com>
2021-10-28 11:59:01 -0400

    java lesson

---
+++ 25OctWeek/4_thursday/code/Rectangle.java
@@ -0,0 +1,9 @@
+public class Rectangle{
+  int width;
+  int height;
+
+  public Rectangle(int w, int h){
+    width = w;
+    height = h;
+  }
+}
---
+++ 25OctWeek/4_thursday/code/RectangleTester.java
@@ -0,0 +1,26 @@
+public class RectangleTester
+{
+    public static void main(String[] args)
+    {
+
+         Rectangle r1 = new Rectangle(10, 2);
+         System.out.println(r1);
+
+         Rectangle r2 = new Rectangle(10, 2);
+         System.out.println(r2);
+
+
+
+         System.out.println(r1 == r2);
+
+         Rectangle r3 = r2;
+         System.out.print(r2 == r3);
+
+         /*
+         String str1 = "I love java.";
+         String str2 = new String("I love java.");
+         System.out.println(str1 == str2);
+         System.out.print(str1.equals(str2));
+         */
+    }
+}
