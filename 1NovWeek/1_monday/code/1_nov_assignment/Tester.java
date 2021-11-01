0be1f982e1beeb26ad3a9f02b4c049fbac17575d
Chris <obrien@lehmanhs.com>
2021-11-01 08:34:36 -0400

    morning

--- 1NovWeek/1_monday/code/1_nov_assignment/Tester.java
+++ 1NovWeek/1_monday/code/1_nov_assignment/Tester.java
@@ -13,17 +13,26 @@ public class Tester{
   }
 
   public static void main(String[] args){
+  //teachers
     CSTeacher shuda = new CSTeacher("Mr.", "Shuda", "12345");
     CSTeacher obrien = new CSTeacher("Dr.", "O'Brien", "23456");
 
+    //students
     Student sam = new Student("Sam", "O'Neill", "123456789", obrien);
     Student rafiki = new Student("Rafiki", "Holder", "234567890", obrien);
-
     Student eric = new Student("Eric", "Chau", "345678912", shuda);
     Student faiza = new Student("Faiza", "Khatun", "4567890123", shuda);
 
+    //unit tests 
     sameCSTeacher(sam, rafiki);
     sameCSTeacher(eric, eric);
     sameCSTeacher(rafiki, faiza);
+    /*
+    expected output:
+    Same teacher.
+    Same student. Same teacher.
+    Different teacher.
+    
+    */
   }
 }
