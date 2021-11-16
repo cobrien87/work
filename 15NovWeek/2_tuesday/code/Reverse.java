4fac2c60c13c877489750dd9f48da82ed0f273f0
Chris O'Brien <cobrien528@gmail.com>
2021-11-16 11:44:45 -0500

    lunch

--- 15NovWeek/2_tuesday/code/Reverse.java
+++ 15NovWeek/2_tuesday/code/Reverse.java
@@ -1,10 +1,18 @@
-public class MyProgram{
+import java.util.*;
+public class Reverse{
   public static String reverse(String str){
     // implement reverse method here
-
+    String newString  = "";
+    for (int i = 0 ; i<str.length(); i++){
+      char character = str.charAt(i);
+      newString = character + newString;
+    }
+    return newString;
   }//end reverse
   public static void main(String[] args){
-    String str  = "Rafiki Holder";
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter a sentence to be reversed: ");
+    String str = input.nextLine();
     String reverse = reverse(str);
     System.out.println(reverse);
   }//end main
