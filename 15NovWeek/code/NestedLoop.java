f8f2d76aa032673c54a971363d54bd89477ddaa9
Chris O'Brien <cobrien528@gmail.com>
2021-11-19 11:51:14 -0500

    java

---
+++ 15NovWeek/code/NestedLoop.java
@@ -0,0 +1,12 @@
+public class NestedLoop{
+
+
+  public static void main(String[] args){
+    String counter = "";
+    for (int i =1; i < 6; i++ ){
+      counter += i + " ";
+    }
+
+    System.out.println(counter);
+  }//end main
+}
