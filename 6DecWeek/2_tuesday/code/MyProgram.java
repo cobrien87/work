be98a95053cabf02fd718de12e69bf7534af17dd
Chris O'Brien <cobrien528@gmail.com>
2021-12-07 11:43:50 -0500

    java

---
+++ 6DecWeek/2_tuesday/code/MyProgram.java
@@ -0,0 +1,9 @@
+public class MyProgram{
+
+  public static void main(String[] args){
+    Rectangle rect = new Rectangle(10,5);
+    System.out.println(rect.width);
+    rect.width = 16;
+    System.out.println(rect.width);
+  }
+}
---
+++ 6DecWeek/2_tuesday/code/Rectangle.java
@@ -0,0 +1,9 @@
+public class Rectangle{
+  private int width;
+  private int height;
+
+  public Rectangle(int w , int h ){
+    this.width  = h;
+    this.height = h;
+  }
+}
