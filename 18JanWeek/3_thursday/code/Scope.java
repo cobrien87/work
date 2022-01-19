09125aa05f425c21e2c2cfaf668d35d9b61ea45d
Chris O'Brien <cobrien528@gmail.com>
2022-01-18 19:01:28 -0500

    evening

---
+++ 18JanWeek/3_thursday/code/Scope.java
@@ -0,0 +1,12 @@
+public class Scope{
+  public static void main(String[] args){
+    for (int outer = 0; outer <5; outer++){
+      for (int inner = 0; inner < 5; inner++){
+      }
+      System.out.println(inner * outer);
+
+    }
+
+
+  }
+}
