de100776cf6b105db4f79fa7b910b4a730d613e0
Chris O'Brien <cobrien528@gmail.com>
2022-02-09 08:34:50 -0500

    pptx and lesson plans

---
+++ 7FebWeek/3_wednesday/code/examples.py
@@ -0,0 +1,10 @@
+if class == "science" or class == "math":
+    print("This is a STEM class.")
+else:
+    print("This is not a stem class.")
+
+
+if class == "science" or "math":
+    print("This is a STEM class.")
+else:
+    print("This is not a stem class.")
